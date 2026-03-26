c8e7d22c790cfcca7a44855e564653dfac23a3ba
Lucas Souza <souza.lucasp10@gmail.com>
2026-03-26 14:25:31 -0300

    New Feature – Adiciona estrutura de pacotes DDD
    - controller: endpoints REST e DTOs
    - domain: entidades, repositórios e exceptions
    - service: regras de negócio e serviços externos
    - infrastructure: configurações e clientes externos

---
+++ src/main/java/com/souzadev/transferenciasapi/TransferenciasapiApplication.java
@@ -0,0 +1,13 @@
+package com.souzadev.transferenciasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferenciasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferenciasapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souzadev/transferenciasapi/TransferenciasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzadev.transferenciasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferenciasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
